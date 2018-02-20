d6973cdff5c7c5243175037b0bc1a9e5c6675cf3
ejaprja <prateek.j.jain@ericsson.com>
2018-02-20 17:51:36 +0000

    added swarm app.

---
+++ src/main/java/com/pjain/app/JaxRsActivator.java
@@ -0,0 +1,10 @@
+package com.pjain.app;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author prateek
+ */
+@ApplicationPath("/")
+public class JaxRsActivator extends Application {
+}
