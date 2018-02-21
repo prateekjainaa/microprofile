0c260d483102d7c3e8f23e5a9c9d4087ae9dbcaf
prateek jain <pjain@localhost.localdomain>
2018-02-21 04:38:12 +0000

    not working.

---
+++ src/main/java/com/pjain/rest/HelloHealthCheck.java
@@ -0,0 +1,18 @@
+package com.pjain.rest;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Health
+@ApplicationScoped
+public class HelloHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("another successful check.").up().build();
+    }
+    
+
+}
