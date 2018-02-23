5920454243a2d68ac91473ba6e10161e6122f55c
prateek jain <pjain@localhost.localdomain>
2018-02-23 18:57:00 +0000

    switched to kumuluz. wildfly sucks.

---
+++ src/main/java/com/pjain/app/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package com.pjain.app;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/my-app")
+public class JaxRsActivator extends Application {
+
+}
